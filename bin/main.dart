6569d027579e62d6f8ac1c6a0973ffc43718aed6
Kasoke <alvarogfn@hotmail.com>
2021-12-19 12:31:54 -0300

    autentication using oauth2

--- bin/main.dart
+++ bin/main.dart
@@ -1,7 +1,28 @@
+import 'dart:convert';
 import 'dart:io';
 import 'package:dotenv/dotenv.dart' show load, clean, isEveryDefined, env;
+import 'package:http/http.dart' as http;
 
 main(List<String> parameters) {
   load();
-  print('your home directory is: ${env['HOME']}');
+  String discordWebHook = env['WEBHOOK']!;
+  String twitterBearer = env['TWITTER_BEARER_TOKEN']!;
+
+  // http.post(
+  //   Uri.parse(discordWebHook),
+  //   headers: {'Content-Type': 'application/json'},
+  //   body: json.encode({
+  //     'content': 'oi',
+  //     'username': 'teste2',
+  //     'avatar_url': 'https://upload.wikimedia.org/wikipedia/commons/8/83/Bra-Cos_%281%29_%28cropped%29.jpg'
+  //   }),
+  // );
+
+  // http.get(
+  //   Uri.parse(
+  //       "https://api.twitter.com/2/tweets/search/recent?query=from:twitterdev"),
+  //   headers: {
+  //     'Authorization': 'Bearer $twitterBearer',
+  //   },
+  // ).then((r) => print(r.body));
 }
